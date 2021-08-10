42da62780cccc72f75e4808661ded14875cef201
Taek Jin Han <tj.han@yale.edu>
2021-08-10 18:23:23 -0400

    getting one pizza

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 /* DOM Element */
 
-const pizzaName = document.querySelector("body > main > h1")
+const pizzaName = document.querySelector("#pizza-name")
+const pizzaRestaurant = document.querySelector("#restaurant-name")
+const pizzaDescription = document.querySelector("#pizza-description")
+const pizzaPrice = document.querySelector("#pizza-price")
+const pizzaImg = document.querySelector("img")
+
+const typeName = document.querySelector("#type-name")
 
 /* Render Functions */ 
 
 const renderPizzaDetails = pizzaObj => {
+    pizzaImg.src = pizzaObj.image_url
     pizzaName.textContent = pizzaObj.name
+    pizzaRestaurant.textContent = pizzaObj.restaurant.name
+    pizzaDescription.textContent = pizzaObj.description
+    pizzaPrice.textContent = pizzaObj.price
+}
+
+const renderType = allTypes => {
+    const li = document.createElement("li")
+    li.textContent = `${allTypes.name}`
+    typeName.append(li)
 }
 
 /* Fatch Functions */ 
-
-// const getOnePizza = id => {
-//     fetch('http://localhost:3000/api/v1/pizzas/${id}')
-//         .then(r => r.json())
-//         .then(console.log)
-// }
 
 const getOnePizza = async (id) => {
     const url = `http://localhost:3000/api/v1/pizzas/${id}`
@@ -23,6 +33,14 @@ const getOnePizza = async (id) => {
     renderPizzaDetails(pizzaObj)
 }
 
-/* Initialize */
+const getAllTypes = async (all) => {
+    const url = `http://localhost:3000/api/v1/types`
+    const response = await fetch(url)
+    const typeAllObj = await response.json()
+    const allTypes = await resp.json()
+    allTypes.forEach(type => renderType(type))
+}
 
-getOnePizza(1)
+/* Initialize */
+getAllTypes()
+getOnePizza(31)
