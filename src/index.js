8df404920c85244eace92ae1dfeb5638a2719fb8
Taek Jin Han <tj.han@yale.edu>
2021-08-11 00:24:24 -0400

    viewing

--- src/index.js
+++ src/index.js
@@ -1,46 +1,101 @@
 /* DOM Element */
 
-const pizzaName = document.querySelector("#pizza-name")
-const pizzaRestaurant = document.querySelector("#restaurant-name")
-const pizzaDescription = document.querySelector("#pizza-description")
-const pizzaPrice = document.querySelector("#pizza-price")
-const pizzaImg = document.querySelector("img")
-
 const typeName = document.querySelector("#type-name")
+
+const cardContainer = document.querySelector("#card-container")
 
 /* Render Functions */ 
 
 const renderPizzaDetails = pizzaObj => {
-    pizzaImg.src = pizzaObj.image_url
+    console.log(pizzaObj)
+
+    const cardDiv = document.createElement("div")
+    cardDiv.id = "card"
+
+    const pizzaImg = document.createElement("img")
+    pizzaImg.src = pizzaObj.image_url 
+    pizzaImg.alt = pizzaObj.name
+
+    const pizzaName = document.createElement("h1")
+    pizzaName.id = "pizza-name"
     pizzaName.textContent = pizzaObj.name
-    pizzaRestaurant.textContent = pizzaObj.restaurant.name
+
+    const restaurantName = document.createElement("h3")
+    restaurantName.id = "restaurant-name" 
+    restaurantName.textContent = pizzaObj.restaurant.name
+
+    const pizzaDescription = document.createElement("p")
+    pizzaDescription.id = "pizza-description"
     pizzaDescription.textContent = pizzaObj.description
-    pizzaPrice.textContent = pizzaObj.price
+
+    const pizzaPrice = document.createElement("p")
+    pizzaPrice.id = "pizza-price"
+    pizzaPrice.textContent = "Price: $" + pizzaObj.price
+
+    const ratingCommentDiv = document.createElement("div")
+    pizzaObj.user_pizzas.forEach(userPizza => {
+        const userPizzaDiv = document.createElement("div")
+        userPizzaDiv.className = "pizza-rating-comment"
+
+        const pizzaRating = document.createElement("p")
+        pizzaRating.className = "pizza-rating"
+        pizzaRating.textContent = "Rating: " + userPizza.rating
+    
+        const pizzaComment = document.createElement("p")
+        pizzaComment.className = "pizza-comment"
+        pizzaComment.textContent = "Comment: " + userPizza.comment
+
+        userPizzaDiv.append(pizzaRating, pizzaComment) 
+        ratingCommentDiv.append(userPizzaDiv)
+    })
+    // create button -> event listener -> bring a form -> append the form to ->  
+
+
+    cardDiv.append(pizzaImg, pizzaName, pizzaDescription, pizzaPrice, ratingCommentDiv)
+
+    cardContainer.append(cardDiv)
+
 }
 
-const renderType = allTypes => {
+function handleTypeClick(type) {
+    console.log(type)
+    cardContainer.innerHTML = ""
+    type.pizzas.forEach(getOnePizza)
+}
+
+const renderType = type => {
     const li = document.createElement("li")
-    li.textContent = `${allTypes.name}`
+    li.textContent = `${type.name}`
+    li.addEventListener('click', e => handleTypeClick(type)) 
     typeName.append(li)
 }
 
+
+
 /* Fatch Functions */ 
 
-const getOnePizza = async (id) => {
-    const url = `http://localhost:3000/api/v1/pizzas/${id}`
+const getOnePizza = async (pizza) => {
+    const url = `http://localhost:3000/api/v1/pizzas/${pizza.id}`
     const response = await fetch(url)
     const pizzaObj = await response.json()
+    console.log(pizzaObj)
     renderPizzaDetails(pizzaObj)
 }
 
 const getAllTypes = async (all) => {
     const url = `http://localhost:3000/api/v1/types`
     const response = await fetch(url)
-    const typeAllObj = await response.json()
-    const allTypes = await resp.json()
-    allTypes.forEach(type => renderType(type))
+    const allTypes = await response.json()
+    // allTypes.forEach(type => renderType(type))
+    let i = 0
+    while(i < 5){ 
+        let max = allTypes.length - 1
+        let num = Math.floor(Math.random() * max)
+        renderType(allTypes[num]) 
+        allTypes.splice(num, 1)
+        i++ 
+    }
 }
 
 /* Initialize */
-getAllTypes()
-getOnePizza(31)
+// getAllTypes()
